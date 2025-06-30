a6ffbab7fff649a92cdb9d0a739612518f740a29
Hector Sanchez <sanchez.hector.m@gmail.com>
2025-06-30 14:14:36 -0400

    Adds register route and the temp dashboard's

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent) },
+  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }
+];
